d241a45556e5457f3864104dbe63c8ae8fef4ea6
AkshayPandit <pandit.akshay14@gmail.com>
2022-01-17 18:24:51 -0600

    bot created

--- telegram_bot.py
+++ telegram_bot.py
@@ -7,21 +7,50 @@ Created on Sun Jan 16 22:14:34 2022
 #import logging library to debug the program better
 
 import logging
-logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
 
 #Import necessary libraries
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
+#importing relevant libraries from selenium
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
-updater = Updater(token='5018778507:AAHBd74vwtAYTCx3z-vNmc81ed7bRR08hUE', use_context=True)
-dispatcher = updater.dispatcher
+import os
+import time
+os.chdir(r"D:\telegram_bot")
 
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',level=logging.INFO)
+
+# Start and read commands are created
 def start(update, context):
     context.bot.send_message(chat_id=update.message.chat_id, 
     text="I'm a bot, please talk to me!")
     
-dispatcher.add_handler(CommandHandler('start', start))
+def read(update, context):
 
+    browser = webdriver.Chrome(os.getcwd() + '\chromedriver.exe')
+    browser.get('https://www.outline.com')
+
+    linkbar = browser.find_element_by_id('source')
+    linkbar.send_keys(context.args) 
+    linkbar.send_keys(Keys.ENTER)
+
+    time.sleep(10)
+    context.bot.send_message(chat_id=update.message.chat_id, text=browser.current_url)
+    
+def main():
+   #updater and dispatcher objects created
+   updater = Updater(token='5018778507:AAHBd74vwtAYTCx3z-vNmc81ed7bRR08hUE', use_context=True)
+   dispatcher = updater.dispatcher
+   
+   dispatcher.add_handler(CommandHandler('start', start))
+   dispatcher.add_handler(CommandHandler('read', read))
+   #start the bot
+   updater.start_polling()
+   #stop
+   updater.idle()
+   
+if __name__ == '__main__':
+       main()
